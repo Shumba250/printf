c2f9a8bb4cf3abb9703943e72d02b2a2e4da854f
Shumba250 <mudakikwaaimable05@gmail.com>
2022-05-20 10:23:57 +0200

    a convesion specifier that prints a reversed string

---
+++ prints_revesersed_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_reverse - writes the str in reverse
+ * @arguments: input string
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed.
+ */
+int print_reverse(va_list arguments, char *buf, unsigned int ibuf)
+{
+	char *str;
+	unsigned int z;
+	int a = 0;
+	char nill[] = "(llun)";
+
+	str = va_arg(arguments, char *);
+	if (str == NULL)
+	{
+		for (z = 0; nill[z]; z++)
+			ibuf = handl_buf(buf, nill[z], ibuf);
+		return (6);
+	}
+	for (z = 0; str[z]; z++)
+		;
+	a = z - 1;
+	for (; a >= 0; a--)
+	{
+		ibuf = handl_buf(buf, str[a], ibuf);
+	}
+	return (z);
+}
