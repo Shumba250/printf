76965f8a5699444fc6ca8cc698991d1f6dcdb121
Shumba250 <mudakikwaaimable05@gmail.com>
2022-05-22 10:10:21 +0200

    deleted files

--- main.h
+++ main.h
@@ -48,7 +48,6 @@ int prinnoct(va_list arguments, char *buf, unsigned int ibuf);
 int prinnhex(va_list arguments, char *buf, unsigned int ibuf);
 int prinnupx(va_list arguments, char *buf, unsigned int ibuf);
 int prinsint(va_list arguments, char *buf, unsigned int ibuf);
-int get_width(const char *format, int *i, va_list list);
 int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
 int ev_print_func(const char *s, int index);
 int is_digit(char);
