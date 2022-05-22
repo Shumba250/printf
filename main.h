1a07f9e41f8e164a328b779f4de2b4d70dbd0063
Shumba250 <mudakikwaaimable05@gmail.com>
2022-05-22 08:47:27 +0200

    added a file

--- main.h
+++ main.h
@@ -51,6 +51,7 @@ int prinsint(va_list arguments, char *buf, unsigned int ibuf);
 int get_width(const char *format, int *i, va_list list);
 int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
 int ev_print_func(const char *s, int index);
+int is_digit(char);
 unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
 int print_buf(char *buf, unsigned int nbuf);
 char *fill_binary_array(char *binary, long int int_in, int isneg, int limit);
